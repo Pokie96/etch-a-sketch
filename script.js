b08837938ce88068d554168c0b594c634311065f
Adam <19adamchristie96@gmail.com>
2022-11-09 19:53:26 +0000

    Restart process from scratch because need to rethink how to accomplish

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 const container = document.getElementById('container')
-let rowNumber = 0;
 
-for(i=0; i<256; i++){
-    if(i % 16 == 0){
-        rowNumber += 1;
-    }
-    let divEl = document.createElement('div');
-    divEl.classList.add(`div-el-row${rowNumber}`, 'div-el');
-    container.appendChild(divEl);
-}
+
