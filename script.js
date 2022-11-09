606f14038346f9e95145a63a6e29af2a609e19a7
Adam <19adamchristie96@gmail.com>
2022-11-09 20:40:54 +0000

    Create new function to create a grid of specified size

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const container = document.getElementById('container')
+const container = document.getElementById('container');
+const rows = document.getElementsByClassName('row')
+
+function createGrid(gridSize){
+    createRows(gridSize);
+    createCells(gridSize);
+}
+
+function createRows(rowNumber){
+    for(i = 0; i < rowNumber; i++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+    }
+}
+
+function createCells(cellNumber){
+    for(j = 0; j < rows.length; j++){
+        for(i = 0; i < cellNumber; i++){
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            let row = rows[j];
+            row.appendChild(cell);
+        }
+    }
+}
+
+createGrid(16);
 
 
